421dd197c72da30b9fec1e4efcaf77945f973ae6
Riana Battick <89482956+rianabattick@users.noreply.github.com>
2024-02-15 18:39:33 -0500

    get value for lowercase letter

--- mips.s
+++ mips.s
@@ -29,5 +29,15 @@ loop_whole_string
   beq $t8, $t4, process_substring
   ble $t0, $t8, is_num_or_letter
   j continue_loop
+
+is_num_or_letter:
+  bge $t8, $t1, is_lower
+  j continue_loop
+
+is_lower:
+  bge $t8, $t2, is_upper
+  sub $t8, $t8, $t1
+  addi $t8, $t8, 10
+  j calculate_sum
   
   
